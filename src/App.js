41560429b28348b281836b79268b46637f9f2e97
Mohak-Trivedi <trivedimohak@gmail.com>
2023-12-22 13:49:00 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,118 @@ import "./styles.css";
 
 
 
-const User = ({ closeModal }) => {
+// const User = ({ closeModal }) => {
 
-  const handleSubmit = (event) => {
-    event.preventDefault(); // Prevent default form submission
-    closeModal(true); // Close the modal
+//   const handleSubmit = (event) => {
+//     event.preventDefault(); // Prevent default form submission
+//     closeModal(true); // Close the modal
+//   };
+
+//   return (
+//     <div className="modal">
+//       <div className="modal-content">
+//         <h2>Fill Details</h2>
+//         <form onSubmit={handleSubmit}>
+//           <div className="form-group">
+//             <label htmlFor="username">Username:</label>
+//             <input
+//               type="text"
+//               id="username"
+//               name="username"
+//               required
+//               className="input-field"
+//             />
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="email">Email Address:</label>
+//             <input
+//               type="email"
+//               id="email"
+//               name="email"
+//               required
+//               className="input-field"
+//             />
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="phone">Phone Number:</label>
+//             <input
+//               type="tel"
+//               id="phone"
+//               name="phone"
+//               required
+//               className="input-field"
+//             />
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="dob">Date of Birth:</label>
+//             <input
+//               type="date"
+//               id="dob"
+//               name="dob"
+//               required
+//               className="input-field"
+//             />
+//           </div>
+//           <div className="form-group">
+//             <button type="submit" className="submit-button">
+//               Submit
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+const User = ({ closeModal }) => {
+  // ... existing code ...
+
+  // Function to validate email
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
   };
 
+  // Function to validate phone number
+  const validatePhone = (phone) => {
+    return phone.length === 10 && !isNaN(phone);
+  };
+
+  // Function to validate date of birth
+  const validateDOB = (dob) => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    return birthDate < today;
+  };
+
+  // Updated handleSubmit function
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const email = formData.get('email');
+    const phone = formData.get('phone');
+    const dob = formData.get('dob');
+
+    if (!validateEmail(email)) {
+      alert('Invalid email. Please check your email address.');
+      return;
+    }
+
+    if (!validatePhone(phone)) {
+      alert('Invalid phone number. Please enter a 10-digit phone number.');
+      return;
+    }
+
+    if (!validateDOB(dob)) {
+      alert('Invalid date of birth. Date of birth cannot be in the future.');
+      return;
+    }
+
+    closeModal(true);
+  };
+
+  // ... rest of the User component ...
   return (
     <div className="modal">
       <div className="modal-content">
@@ -66,6 +171,7 @@ const User = ({ closeModal }) => {
     </div>
   );
 };
+
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
